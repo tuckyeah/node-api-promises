57816f54ce5687201d0360fa2a198cbdc6bd4c33
Antony Donovan <antony.donovan@generalassemb.ly>
2016-08-24 13:22:44 -0400

    Refresh for cohort 013
    Update from node-template

--- spec/example.spec.js
+++ spec/example.spec.js
@@ -1,9 +1,37 @@
 'use strict';
 
-var example = require('../lib/example');
+// Allow chai syntax like `expect(foo).to.be.ok;`
+// jshint -W030
 
-describe('Example', () => {
-  it('is true', () => {
-    expect(example()).toBe(true);
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+
+chai.use(chaiAsPromised);
+
+const expect = chai.expect;
+
+const example = require('../lib/example');
+
+describe('Sync', function () {
+  it('is true', function () {
+    expect(example.sync(true)).to.be.true;
+  });
+});
+
+describe('Async', function () {
+  it('is true', function (done) {
+    example.async(true, function (error, value) {
+      if (error || value !== true) {
+        error = error || new Error(`value is ${value}`);
+      }
+
+      done(error);
+    });
+  });
+});
+
+describe('Promise', function () {
+  it('is true', function () {
+    expect(example.promise(true)).to.eventually.be.true;
   });
 });
